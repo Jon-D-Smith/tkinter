5d324a1637b7106344639341e460ce210384ef9b
Jon Smith <aardvarkjon94@yahoo.com>
2021-10-25 12:50:50 -0500

    Buttons added in layout

--- viewer.py
+++ viewer.py
@@ -8,10 +8,18 @@ root.iconbitmap('panda.ico')
 img1 = ImageTk.PhotoImage(Image.open('images/panda.png'))
 img2 = ImageTk.PhotoImage(Image.open('images/Headshot.jpg'))
 img3 = ImageTk.PhotoImage(Image.open('images/images.jpg'))
-my_label = Label(image=img1).pack()
 
+image_list = [img1, img2, img3]
 
+my_label = Label(image=image_list[0])
+my_label.grid(row=0,column=0, columnspan=3)
+
+# buttons
+button_back = Button(root, text="<<")
+button_back.grid(row=1,column=0)
 button_quit = Button(root, text="Exit program", command=root.quit)
-button_quit.pack()
+button_quit.grid(row=1, column=1)
+button_forward = Button(root, text=">>")
+button_forward.grid(row=1, column=2)
 
 root.mainloop()
