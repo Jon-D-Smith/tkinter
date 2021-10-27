48713dc65b9d1fcd73acd106f3f3f4b7c7752eef
Jon Smith <aardvarkjon94@yahoo.com>
2021-10-27 18:02:37 -0500

    Added status bar Label to the bottom of the page

---
+++ status.py
@@ -0,0 +1,63 @@
+from tkinter import *
+from PIL import ImageTk, Image 
+
+root = Tk()
+root.title('Icons will be icons')
+root.iconbitmap('panda.ico')
+
+img1 = ImageTk.PhotoImage(Image.open('images/panda.png'))
+img2 = ImageTk.PhotoImage(Image.open('images/Headshot.jpg'))
+img3 = ImageTk.PhotoImage(Image.open('images/images.jpg'))
+
+image_list = [img1, img2, img3]
+
+status = Label(root, text="Image 1 of " + str(len(image_list)))
+
+my_label = Label(image=image_list[0])
+my_label.grid(row=0,column=0, columnspan=3)
+
+def forward(image_number):
+    global my_label
+    global button_forward
+    global button_back
+    
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: forward(image_number-1))
+
+    if image_number == len(image_list):
+        button_forward = Button(root, text=">>", state=DISABLED)
+    
+    my_label.grid(row=0,column=0, columnspan=3)
+    button_forward.grid(row=1,column=2)
+    button_back.grid(row=1, column=0)
+
+def back(image_number):
+    global my_label
+    global button_forward
+    global button_back
+    
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number+1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: forward(image_number-1))
+
+    if image_number == 1:
+        button_forward = Button(root, text=">>", state=DISABLED)
+    
+    my_label.grid(row=0,column=0, columnspan=3)
+    button_forward.grid(row=1,column=2)
+    button_back.grid(row=1, column=0)
+
+# buttons
+button_back = Button(root, text="<<", command=lambda: back(0), state=DISABLED)
+button_back.grid(row=1,column=0)
+button_quit = Button(root, text="Exit program", command=root.quit)
+button_quit.grid(row=1, column=1)
+button_forward = Button(root, text=">>", command=lambda: forward(2))
+button_forward.grid(row=1, column=2)
+
+status.grid(row=2, column=0, columnspan=3)
+
+root.mainloop()
