f050a2bb97d57c5eb09dc2f663203edf29979f0e
Jon Smith <aardvarkjon94@yahoo.com>
2021-10-27 18:11:31 -0500

    Positioned status bar, and added logic

--- status.py
+++ status.py
@@ -11,7 +11,7 @@ img3 = ImageTk.PhotoImage(Image.open('images/images.jpg'))
 
 image_list = [img1, img2, img3]
 
-status = Label(root, text="Image 1 of " + str(len(image_list)))
+status = Label(root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
 
 my_label = Label(image=image_list[0])
 my_label.grid(row=0,column=0, columnspan=3)
@@ -33,31 +33,37 @@ def forward(image_number):
     button_forward.grid(row=1,column=2)
     button_back.grid(row=1, column=0)
 
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 def back(image_number):
     global my_label
     global button_forward
     global button_back
     
     my_label.grid_forget()
-    my_label = Label(image=image_list[image_number+1])
+    my_label = Label(image=image_list[image_number-1])
     button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
     button_back = Button(root, text="<<", command=lambda: forward(image_number-1))
 
     if image_number == 1:
-        button_forward = Button(root, text=">>", state=DISABLED)
+        button_back = Button(root, text=">>", state=DISABLED)
     
     my_label.grid(row=0,column=0, columnspan=3)
     button_forward.grid(row=1,column=2)
     button_back.grid(row=1, column=0)
 
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 # buttons
-button_back = Button(root, text="<<", command=lambda: back(0), state=DISABLED)
+button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_back.grid(row=1,column=0)
 button_quit = Button(root, text="Exit program", command=root.quit)
 button_quit.grid(row=1, column=1)
 button_forward = Button(root, text=">>", command=lambda: forward(2))
-button_forward.grid(row=1, column=2)
+button_forward.grid(row=1, column=2, pady=10)
 
-status.grid(row=2, column=0, columnspan=3)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 root.mainloop()
