45c9158f6bd2416f34f67a2b4acdf3dc4b77c742
Jon Smith <aardvarkjon94@yahoo.com>
2021-10-28 19:05:39 -0500

    Fixed bug where it would keep going back forever

--- status.py
+++ status.py
@@ -17,44 +17,47 @@ my_label = Label(image=image_list[0])
 my_label.grid(row=0,column=0, columnspan=3)
 
 def forward(image_number):
-    global my_label
-    global button_forward
-    global button_back
-    
-    my_label.grid_forget()
-    my_label = Label(image=image_list[image_number-1])
-    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
-    button_back = Button(root, text="<<", command=lambda: forward(image_number-1))
+	global my_label
+	global button_forward
+	global button_back
 
-    if image_number == len(image_list):
-        button_forward = Button(root, text=">>", state=DISABLED)
-    
-    my_label.grid(row=0,column=0, columnspan=3)
-    button_forward.grid(row=1,column=2)
-    button_back.grid(row=1, column=0)
+	my_label.grid_forget()
+	my_label = Label(image=image_list[image_number-1])
+	button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+	button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+	
+	if image_number == 3:
+		button_forward = Button(root, text=">>", state=DISABLED)
 
-    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
-    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+	my_label.grid(row=0, column=0, columnspan=3)
+	button_back.grid(row=1, column=0)
+	button_forward.grid(row=1, column=2)
+
+	status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+	status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+	
+		
 
 def back(image_number):
-    global my_label
-    global button_forward
-    global button_back
-    
-    my_label.grid_forget()
-    my_label = Label(image=image_list[image_number-1])
-    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
-    button_back = Button(root, text="<<", command=lambda: forward(image_number-1))
+	global my_label
+	global button_forward
+	global button_back
 
-    if image_number == 1:
-        button_back = Button(root, text=">>", state=DISABLED)
-    
-    my_label.grid(row=0,column=0, columnspan=3)
-    button_forward.grid(row=1,column=2)
-    button_back.grid(row=1, column=0)
+	my_label.grid_forget()
+	my_label = Label(image=image_list[image_number-1])
+	button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+	button_back = Button(root, text="<<", command=lambda: back(image_number-1))
 
-    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
-    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+	if image_number == 1:
+		button_back = Button(root, text="<<", state=DISABLED)
+
+	my_label.grid(row=0, column=0, columnspan=3)
+	button_back.grid(row=1, column=0)
+	button_forward.grid(row=1, column=2)
+
+	# Update Status Bar
+	status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+	status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 # buttons
 button_back = Button(root, text="<<", command=back, state=DISABLED)
