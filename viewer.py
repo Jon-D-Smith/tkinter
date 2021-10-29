7cf851968e468fdb2688c8d0a3b383ce9702b173
Jon Smith <aardvarkjon94@yahoo.com>
2021-10-28 19:08:30 -0500

    Fixed bug in viewer

--- viewer.py
+++ viewer.py
@@ -15,41 +15,41 @@ my_label = Label(image=image_list[0])
 my_label.grid(row=0,column=0, columnspan=3)
 
 def forward(image_number):
-    global my_label
-    global button_forward
-    global button_back
-    
-    my_label.grid_forget()
-    my_label = Label(image=image_list[image_number-1])
-    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
-    button_back = Button(root, text="<<", command=lambda: forward(image_number-1))
+	global my_label
+	global button_forward
+	global button_back
 
-    if image_number == len(image_list):
-        button_forward = Button(root, text=">>", state=DISABLED)
-    
-    my_label.grid(row=0,column=0, columnspan=3)
-    button_forward.grid(row=1,column=2)
-    button_back.grid(row=1, column=0)
+	my_label.grid_forget()
+	my_label = Label(image=image_list[image_number-1])
+	button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+	button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+	
+	if image_number == 3:
+		button_forward = Button(root, text=">>", state=DISABLED)
 
+	my_label.grid(row=0, column=0, columnspan=3)
+	button_back.grid(row=1, column=0)
+	button_forward.grid(row=1, column=2)
+    
 def back(image_number):
-    global my_label
-    global button_forward
-    global button_back
-    
-    my_label.grid_forget()
-    my_label = Label(image=image_list[image_number+1])
-    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
-    button_back = Button(root, text="<<", command=lambda: forward(image_number-1))
+	global my_label
+	global button_forward
+	global button_back
 
-    if image_number == 1:
-        button_forward = Button(root, text=">>", state=DISABLED)
-    
-    my_label.grid(row=0,column=0, columnspan=3)
-    button_forward.grid(row=1,column=2)
-    button_back.grid(row=1, column=0)
+	my_label.grid_forget()
+	my_label = Label(image=image_list[image_number-1])
+	button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+	button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+	if image_number == 1:
+		button_back = Button(root, text="<<", state=DISABLED)
+
+	my_label.grid(row=0, column=0, columnspan=3)
+	button_back.grid(row=1, column=0)
+	button_forward.grid(row=1, column=2)
 
 # buttons
-button_back = Button(root, text="<<", command=lambda: back(0), state=DISABLED)
+button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_back.grid(row=1,column=0)
 button_quit = Button(root, text="Exit program", command=root.quit)
 button_quit.grid(row=1, column=1)
